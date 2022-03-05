31892fe2079ee885401a47943e95695f4750b268
MeherKharbachi <mkharbachi@ats-digital.com>
2022-03-05 23:03:02 +0100

    Change Intro page

--- _build/jupyter_execute/Intro.py
+++ _build/jupyter_execute/Intro.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Introduction
+# # A multi-task learning approach to predict the outcome of a football game using human experts input
 
-# Welcome to my PFE book
+# In this work, we propose to exploit a new type of data to capture the game environment. We will extend the methodology described in and build a multi-head multi task deep learning network able to digest both Text data provided by human football experts and more standard time series tabular data. The results will be compared to traditional and machine learning approaches described in the literature.
